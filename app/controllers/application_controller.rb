f8234a5bc41d4d1f6e52c053f087b85a4e95f656
Hannada <dphanna@gmail.com>
2019-10-02 13:30:56 -0400

    Cell and figvaper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :signed_in?
+
+    private 
+
+    def current_user
+        @current_user ||= user.find_by(session_token: session[:session_token])
+    end
+
+    def logged_in?
+        !!current_user
+    end 
+
+
+    def login!(user)
+        @current_user = user 
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout
+        current_user.reset_session_token!
+        @current_user = nil
+        session[:session_token] = nil 
+    end 
+
+    def ensure_logged_in!
+        redirect_to new_session_url unless logged_in? 
+    end 
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     validates :username, :email, :password_digest, :session_token, presence: true 
     validates :password, length: {minimum: 6, allow_nil: true}
+    
     after_initialize :ensure_session_token
     attr_reader :password
 

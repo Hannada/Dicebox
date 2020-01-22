967d479e24c69d1c451c362c189014f4ac625e59
Hannada <dphanna@gmail.com>
2020-01-22 13:40:26 -0500

    Sidebar prototype

--- frontend/components/video/video_show.jsx
+++ frontend/components/video/video_show.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { recieveCurrentUser } from "../../actions/session_actions";
 import Welcome  from "../welcome/welcome"
 import WelcomeContainer from "../welcome/welcome_container"
+import VideoIndexContainer from "../video/video_index_container";
+
 
 class VideoShow extends React.Component {
     constructor(props){
@@ -22,12 +24,15 @@ class VideoShow extends React.Component {
         return(
             <div className="video-show-main">
                 <WelcomeContainer/>
-                <video width="1000" height="550" controls>
+                <video className="main-vid" width="1000" height="550" controls>
                     <source src={video.vidUrl} type="video/mp4"/>
                 </video>
                 <div className="vid-title"><img src={window.diceLoginUrl} width="20" height="20" />{video.title}</div>
                 {/* <div>{recieveCurrentUser(video.user_id)}</div> */}
+                <div> {video.user_id}</div>
                 <div className="vid-description">{video.description}</div>
+
+                <div className="sidebar-index"><VideoIndexContainer/></div>
             </div>
         )
     }

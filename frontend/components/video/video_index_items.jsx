253d33c24429ec8822567083197d0b4bbe1fa892
Hannada <dphanna@gmail.com>
2020-02-13 14:18:47 -0500

    Initial alterations to item result in video being undefined

--- frontend/components/video/video_index_items.jsx
+++ frontend/components/video/video_index_items.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 
 
 const VideoIndexItem = ({ video }) => {
+  
+
     return (
         <div className="index-items">
             <Link to={`/videos/${video.id}`} className="index-item"> 
@@ -18,6 +20,27 @@ const VideoIndexItem = ({ video }) => {
             </Link>
         </div>);
 };
+
+// const VideoIndexItem = (props, {video} )=> {
+
+
+
+
+//         return (
+//         <div className="index-items">
+//             <Link to={`/videos/${props.video.id}`} className="index-item"> 
+//                 <video width="270.66" height="151.86">
+//                     <source src={video.vidUrl} type="video/mp4" />
+//                 </video>
+//                 <div className="index-vid-text">
+//                     <p className="index-title">{video.title}</p>
+//                     {/* <p>{video.user_name}</p> */}
+//                     {/* <p>views</p> */}
+//                 </div>
+//             </Link>
+//         </div>);
+    
+// }
 
 
 // Preparing to alter index item. Item currently doesnt have access to user_id 

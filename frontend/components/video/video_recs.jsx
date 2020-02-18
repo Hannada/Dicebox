38789b008073240926f13a1754ced79f953dc09b
Hannada <dphanna@gmail.com>
2020-02-18 15:40:53 -0500

    Color change

--- frontend/components/video/video_recs.jsx
+++ frontend/components/video/video_recs.jsx
@@ -7,14 +7,14 @@ const VideoRecs = ({ video }) => {
 
 
     return (
-        <div className="index-items">
-            <Link to={`/videos/${video.id}`} className="index-item">
+        <div className="rec-items">
+            <Link to={`/videos/${video.id}`} className="rec-item">
                 <video width="270.66" height="151.86">
                     <source src={video.vidUrl} type="video/mp4" />
                 </video>
-                <div className="index-vid-text">
-                    <p className="index-title">{video.title}</p>
-                    <p className="index-user">{video.user_name}</p>
+                <div className="rec-vid-text">
+                    <p className="rec-title">{video.title}</p>
+                    <p className="rec-user">{video.user_name}</p>
                 </div>
             </Link>
         </div>);

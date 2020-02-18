f97d1be18522335be4990cb13762ecf607969a45
Hannada <dphanna@gmail.com>
2020-02-18 15:37:17 -0500

    Creating potential replacement component

---
+++ frontend/components/video/video_recs.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+const VideoRecs = ({ video }) => {
+
+
+    return (
+        <div className="index-items">
+            <Link to={`/videos/${video.id}`} className="index-item">
+                <video width="270.66" height="151.86">
+                    <source src={video.vidUrl} type="video/mp4" />
+                </video>
+                <div className="index-vid-text">
+                    <p className="index-title">{video.title}</p>
+                    <p className="index-user">{video.user_name}</p>
+                </div>
+            </Link>
+        </div>);
+};
+
+export default VideoRecs;

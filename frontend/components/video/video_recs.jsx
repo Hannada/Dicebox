8f581acf0a58e6f7daa5a5047e7124a68c3285b6
Hannada <dphanna@gmail.com>
2020-02-20 14:09:25 -0500

    Text dec removed

--- frontend/components/video/video_recs.jsx
+++ frontend/components/video/video_recs.jsx
@@ -4,27 +4,37 @@ import { Link } from 'react-router-dom';
 
 
 const VideoRecs = ({video}) => {
-    // const video = props.video 
+ 
 
-
-    // const handleClick = () => {
-    //     props.history.push(`/videos/${video.id}`)
-    // }
+    // return (
+    //     <div className="rec-items">
+    //         <Link to={`/videos/${video.id}`} className="rec-item">
+    //             <div className="rec-vid">
+    //                 <video width="270.66" height="151.86">
+    //                     <source src={video.vidUrl} type="video/mp4" />
+    //                 </video>
+    //             </div>
+    //             <div className="rec-vid-text">
+    //                 <p className="rec-title">{video.title}</p>
+    //                 <p className="rec-user"><img src={window.diceLoginUrl} width="20" height="20" />{video.user_name}</p>
+    //             </div>
+    //         </Link>
+    //     </div>);
 
     return (
-        <div className="rec-items">
+        <div className="index-items">
             <Link to={`/videos/${video.id}`} className="rec-item">
                 <div className="rec-vid">
                     <video width="270.66" height="151.86">
                         <source src={video.vidUrl} type="video/mp4" />
                     </video>
                 </div>
-                <div className="rec-vid-text">
-                    <p className="rec-title">{video.title}</p>
-                    <p className="rec-user"><img src={window.diceLoginUrl} width="20" height="20" />{video.user_name}</p>
+                <div className="index-vid-text">
+                    <p className="index-title">{video.title}</p>
+                    <p className="index-user"><img src={window.diceLoginUrl} width="20" height="20" />{video.user_name}</p>
                 </div>
             </Link>
-        </div>);
+        </div>);    
 };
 
 export default VideoRecs;

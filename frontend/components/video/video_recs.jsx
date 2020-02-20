02bd7ccd7adb3b010df165d50203f1540cdfb7bc
Hannada <dphanna@gmail.com>
2020-02-20 13:54:46 -0500

    Css work but weird lines

--- frontend/components/video/video_recs.jsx
+++ frontend/components/video/video_recs.jsx
@@ -13,10 +13,12 @@ const VideoRecs = ({video}) => {
 
     return (
         <div className="rec-items">
-            <Link to={`/videos/${video.id}`} className="index-item">
-                <video width="270.66" height="151.86">
-                    <source src={video.vidUrl} type="video/mp4" />
-                </video>
+            <Link to={`/videos/${video.id}`} className="rec-item">
+                <div className="rec-vid">
+                    <video width="270.66" height="151.86">
+                        <source src={video.vidUrl} type="video/mp4" />
+                    </video>
+                </div>
                 <div className="rec-vid-text">
                     <p className="rec-title">{video.title}</p>
                     <p className="rec-user"><img src={window.diceLoginUrl} width="20" height="20" />{video.user_name}</p>

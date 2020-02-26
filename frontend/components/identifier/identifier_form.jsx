1089883e3e33c8fa07fa2e9260cf0bff689e7bc1
Hannada <dphanna@gmail.com>
2020-02-26 18:18:36 -0500

    Added error render

--- frontend/components/identifier/identifier_form.jsx
+++ frontend/components/identifier/identifier_form.jsx
@@ -59,6 +59,20 @@ class IdentifierForm extends React.Component {
 
     //Above version thinks verify form isnt a function?
 
+
+
+    renderErrors() { //Might need some work
+        return (
+            <ul>
+                {this.props.errors.map(error => {
+                    return <li>
+                        {error}
+                    </li>
+                })}
+            </ul>
+        )
+    }
+
     render() {
         return (
             <div className="email-main">

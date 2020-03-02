d27dc63b4d48d0009ec7f38af7bbc2d5dbab54cf
Hannada <dphanna@gmail.com>
2020-03-02 15:28:19 -0500

    Switched to errror over errors

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -16,9 +16,9 @@ export const logoutCurrentUser = () => ({
     type: LOGOUT_CURRENT_USER
 })
 
-export const receiveSessionErrors = errors => ({
+export const receiveSessionErrors = error => ({
     type: RECEIVE_SESSION_ERRORS,
-    errors 
+    error
 })
 
 export const checkUserEmail = email => ({
@@ -50,20 +50,20 @@ export const logout = () => dispatch => (
     SessionAPIUtil.logout().then(user => dispatch(logoutCurrentUser()))
 );
 
-// export const checkEmail = email => dispatch => {
-//     return (
-//         SessionAPIUtil.email_check(email).then(email => dispatch(checkUserEmail(email))),
-//         errors => (
-//             dispatch(receiveSessionErrors(errors.responseJSON))
-//         )
-//     )
-// };
+export const checkEmail = email => dispatch => {
+    return (
+        SessionAPIUtil.email_check(email).then(email => dispatch(checkUserEmail(email))),
+        error => (
+            dispatch(receiveSessionErrors(error.responseJSON))
+        )
+    )
+};
 
 //Currently Errors is causing verify form to explode. Looking into methods of testing.
 
-export const checkEmail = email => dispatch => (
-    SessionAPIUtil.email_check(email).then(email => dispatch(checkUserEmail(email)))
-);
+// export const checkEmail = email => dispatch => (
+//     SessionAPIUtil.email_check(email).then(email => dispatch(checkUserEmail(email)))
+// );
 
 export const fetchUser = id => dispatch => (
     SessionAPIUtil.fetchUser(id).then(user => dispatch(receiveCurrentUser(user)))

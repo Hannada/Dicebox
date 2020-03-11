937d8739932808a8fb2eb36144586cb96a8199f0
Hannada <dphanna@gmail.com>
2020-03-11 16:12:33 -0400

    Structured comments controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,2 +1,22 @@
 class Api::CommentsController < ApplicationController
+    def show
+    end
+
+    def index
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private 
+
+    def comment_params 
+        params.require(:comment).permit(:id, :body, :user_id, :video_id)
+    end 
 end

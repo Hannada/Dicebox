476e755be38bec13dd10b2a985439493c153968f
Hannada <dphanna@gmail.com>
2020-03-12 18:23:54 -0400

    Comment form done

--- frontend/components/comments/comment_form.jsx
+++ frontend/components/comments/comment_form.jsx
@@ -11,12 +11,33 @@ class CommentForm extends React.Component{
 
     handleComment(e) {
         e.prevenDefault();
+        if (this.props.currentUser) {
+            this.setState({body: ""})
+            this.props.postComment(this.state, this.props.videoId)
+        }else {
+            this.props.history.push("/")
+        }
 
     }
 
 
     update(field) {
         return e => this.setState({ [field]: e.target.value })
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text" 
+                            fillintext="Add a public comment"
+                            onChange={this.update("body")}
+                            value={this.state.body}
+                    />
+                    <button type="submit">Add Comment</button>
+                </form>
+            </div>
+        )
     }
 }
 

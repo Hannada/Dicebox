9d7f7385e162501517c16d32b02be706050e6e19
Hannada <dphanna@gmail.com>
2020-03-12 12:47:06 -0400

    Reducer done?

--- frontend/reducers/entities/comments_reducer.js
+++ frontend/reducers/entities/comments_reducer.js
@@ -1,2 +1,19 @@
 import { RECEIVE_COMMENT, RECEIVE_ALL_COMMENTS, REMOVE_COMMENT} from "../../actions/comment_action"
 
+const commentsReducer = (state = {}, action) => {
+    Object.freeze(state);
+    switch (action.type) {
+        case RECEIVE_ALL_COMMENTS:
+            return Object.assign({}, state, action.comments)
+        case RECEIVE_COMMENT:
+            return Object.assign({}, state, action.comment)
+        case REMOVE_COMMENT:
+            let newState = Object.assign({}, state);
+            delete newState[action.commentId];
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export default commentsReducer;

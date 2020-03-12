62d8d288f35472a28bff2904148e72f71686bbef
Hannada <dphanna@gmail.com>
2020-03-12 12:48:52 -0400

    Added to entities reducer

--- frontend/reducers/entities/entities_reducer.js
+++ frontend/reducers/entities/entities_reducer.js
@@ -3,10 +3,12 @@ import { combineReducers } from "redux";
 import usersReducer from "./users_reducer";
 import videosReducer from "./videos_reducer";
 import navbarSidebarReducer from "./navbar_sidebar_reducer";
+import commentsReducer from "./comments_reducer";
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
     videos: videosReducer,
+    comments: commentsReducer,
     navbarSidebar: navbarSidebarReducer
 
 });

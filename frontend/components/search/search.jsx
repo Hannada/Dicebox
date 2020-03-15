842adba7ab8224b2aa2c5348f79c652739dd1a85
Hannada <dphanna@gmail.com>
2020-03-14 21:23:50 -0400

    Search component planned out, search item made

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,1 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
 
+class Search extends React.Component {
+    constructor(props){
+        super(props);
+    }
+}
+
+export default Search;

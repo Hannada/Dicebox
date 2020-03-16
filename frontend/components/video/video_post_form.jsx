fb8cb09edb22a08f6aff3bb26245fb29f16348e6
Hannada <dphanna@gmail.com>
2020-03-16 15:45:16 -0400

    Flushing out handle file

--- frontend/components/video/video_post_form.jsx
+++ frontend/components/video/video_post_form.jsx
@@ -5,6 +5,7 @@ class VideoPostForm extends React.Component {
         super(props)
         this.state = this.props.video 
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInput = this.handleInput.bind(this);
     }
 
     handleFile(e){
@@ -15,10 +16,23 @@ class VideoPostForm extends React.Component {
 
     }
 
+    handleInput(){
+
+    }
+
     render() {
         return(
             <form onSubmit={this.handleSubmit}>
-                
+                <label htmlFor="video-body">Body of Video</label>
+                <input type="text"
+                    id="video-body"
+                    value={this.state.title}
+                    onChange={this.handleInput}
+                />
+                <input type="file"
+                    onChange={this.handleFile}
+                />
+                <button>Upload a new video</button>
             </form>
         )
     }

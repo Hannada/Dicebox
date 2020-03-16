48821c3f1e1253915f7d6c13c903c3cfd836264c
Hannada <dphanna@gmail.com>
2020-03-16 15:39:04 -0400

    Working on video submit form

--- frontend/components/video/video_post_form.jsx
+++ frontend/components/video/video_post_form.jsx
@@ -3,8 +3,26 @@ import React from "react";
 class VideoPostForm extends React.Component {
     constructor(props){
         super(props)
+        this.state = this.props.video 
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-    
+
+    handleFile(e){
+
+    }
+
+    handleSubmit(){
+
+    }
+
+    render() {
+        return(
+            <form onSubmit={this.handleSubmit}>
+                
+            </form>
+        )
+    }
+
 }
 
 export default VideoPostForm;

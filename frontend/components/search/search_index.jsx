0997369143226b3f3d73f1166b53024a2095883d
Hannada <dphanna@gmail.com>
2020-03-18 21:03:17 -0400

    Search index skeleton setup

--- frontend/components/search/search_index.jsx
+++ frontend/components/search/search_index.jsx
@@ -1,3 +1,10 @@
 import React from "react";
 import { SearchIndexItem } from "./search_index_items";
 
+class SearchIndex extends React.Component{
+    constructor(props){
+        super(props)
+    }
+}
+
+export default SearchIndex;

86a5ad0860900cb89b8eb284dc31b962e9020ec1
Hannada <dphanna@gmail.com>
2020-03-19 13:04:36 -0400

    Trying out tutorial method

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { SearchIndexItem } from "./search_index_items";
+
 
 class Search extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            currentSearch: ""
+        }
     }
 
     componentDidMount() {
@@ -19,9 +24,23 @@ class Search extends React.Component {
     }
 
     render(){
+        const searchedVideos = this.props.videos.map(video => {
+            return (
+                <SearchIndexItem
+                    key={video.id}
+                    video={video}
+                />
+            )
+        })
+
+        let filteredSearch = this.props.videos.filter(
+            (video) => {
+                return video.title.indexOf(this.state.currentSearch !== -1)
+            }
+        )
         return (
             <div>
-                
+
             </div>
         )
     }

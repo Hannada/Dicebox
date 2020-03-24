22a624d95b37ece9c4251ddb2c97be57465f8ff1
Hannada <dphanna@gmail.com>
2020-03-23 23:18:24 -0400

    Work on commend index item container

---
+++ frontend/components/comments/comment_index_item.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// const CommentIndexItem = ({comment}) => {
+//     return (
+//         <div></div>
+//     )
+// };
+
+export default CommentIndexItem;

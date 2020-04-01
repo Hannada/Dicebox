b00bf8349951d66135e14cc8d6496c6853d8757b
Hannada <dphanna@gmail.com>
2020-03-31 20:58:39 -0400

    Defining search on model

--- app/models/video.rb
+++ app/models/video.rb
@@ -17,6 +17,16 @@ class Video < ApplicationRecord
             errors[:vid] << "Must be attached"
         end
     end
+
+    def self.search(params)
+        str = "%#{params}"
+        @videos = Video 
+            .joins("LEFT OUTER JOIN users on videos.user_id = users.id")
+            .where(
+                "UPPER(videos.title) LIKE UPPER(:query) OR
+                UPPER(videos.description) LIKE UPPER(:query) OR
+                UPPER(users.username) LIKE UPPER(:query)", query: str)
+    end
     
     #     def self.search(params)
     #    str = "%#{params}%"

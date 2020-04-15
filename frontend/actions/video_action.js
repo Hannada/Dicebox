6f7ba7853e9bee0e3d17fa856d60a7d572c45980
Hannada <dphanna@gmail.com>
2020-04-15 14:22:29 -0400

    Testing out search function in actions

--- frontend/actions/video_action.js
+++ frontend/actions/video_action.js
@@ -11,9 +11,16 @@ export const CLEAR_VIDEO_ERRORS = "CLEAR_ERRORS"; //Trying something else out
 
 
 export const receiveAllVideos = videos => ({
+    type: SEARCH_VIDS,
+    videos
+})
+
+export const receiveSearchVideos = videos => ({
     type: RECEIVE_ALL_VIDEOS,
     videos
 })
+
+// Making a duplicate of recieve all for different results effectively ^
 
 export const receiveCurrentVideo = video => ({
     type: RECEIVE_CURRENT_VIDEO,
@@ -34,10 +41,11 @@ export const clearVideoErrors = () => ({
     type: CLEAR_VIDEO_ERRORS
 })
 
-export const searchVids =  searchResults => ({
-    type: SEARCH_VIDS,
-    searchResults
-})
+// export const searchVids =  searchResults => ({
+//     type: SEARCH_VIDS,
+//     searchResults
+// })
+
 export const clearSearch = () => ({
     type: CLEAR_SEARCH,
 })
@@ -46,9 +54,17 @@ export const clearSearch = () => ({
 //     VideoUtil.fetchVideos().then(videos => dispatch(receiveAllVideos(videos)))
 // )
 
-export const fetchVideos = () => dispatch => (
-    VideoUtil.fetchVideos().then(videos => dispatch(receiveAllVideos(videos)))
-)
+export const fetchVideos = search => dispatch => {
+    if (search) {
+        return VideoUtil.fetchVideos(search)
+            .then(videos => dispatch(receiveSearchVideos(videos)))
+    }else {
+        return VideoUtil.fetchVideos(search)
+            .then(videos => dispatch(receiveAllVideos(videos)))
+    }
+}
+
+// The above ismeant to check for some kind of search params. Current working regardless.
 
 export const fetchVideo = id => dispatch => (
     VideoUtil.fetchVideo(id).then(video => dispatch(receiveCurrentVideo(video)))

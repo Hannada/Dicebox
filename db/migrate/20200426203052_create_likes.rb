6bfa39259c836209b6922951c6491501be407802
Hannada <dphanna@gmail.com>
2020-04-27 13:41:15 -0400

    Added first run of likes schema

--- db/migrate/20200426203052_create_likes.rb
+++ db/migrate/20200426203052_create_likes.rb
@@ -3,8 +3,10 @@ class CreateLikes < ActiveRecord::Migration[5.2]
     create_table :likes do |t|
       t.integer :user_id, null: false
       t.integer :video_id, null: false 
+      t.boolean :liked, null: false 
       
       t.timestamps
     end
+    add_index :likes, [:user_id, :video_id]
   end
 end
